edeb8a77d29aca59ff699f56cbb560f36bcd63ac
karol00-byte <83399380+karol00-byte@users.noreply.github.com>
2021-05-12 17:47:34 +0800

    Update 01.detectup_gh.py

--- 01.detectup_gh.py
+++ 01.detectup_gh.py
@@ -16,14 +16,11 @@ from azure.storage.blob import ContainerClient
 
 #選擇第一支攝影機
 cap = cv2.VideoCapture(0)
-
 cap.set(cv2. CAP_PROP_FRAME_WIDTH, 650)
 cap.set(cv2. CAP_PROP_FRAME_HEIGHT, 500)
 
-
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(".\\shape_predictor_68_face_landmarks.dat\\shape_predictor_68_face_landmarks.dat")
-
 
 while (cap.isOpened()):
 
@@ -41,8 +38,7 @@ while (cap.isOpened()):
         cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 4, cv2.LINE_AA)       
         cv2.putText(frame, text, (x1,y1), cv2.FONT_HERSHEY_DUPLEX, 0.7, (255,255,255),1 ,cv2.LINE_AA)
        
-        landmarks_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        
+        landmarks_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)       
         shape = predictor(landmarks_frame, d)
         
         #繪製68個特徵點 #紅點就是偵測出人臉的68個特徵點
@@ -63,8 +59,7 @@ while (cap.isOpened()):
 cap.release()
 cv2.destroyAllWindows()
 
-
-img = cv2.imread('.\\006.jpg')
+img = cv2.imread('.\\001.jpg')
 img_array = np.array(img)
 #type(img)
 
@@ -76,6 +71,3 @@ image_stream.seek(0)
 container_client = ContainerClient.from_container_url("https://01facetest.blob.core.windows.net/quickstart2578d35b-e0f0-4c75-905a-62fbc7d3def0?sp=rw&st=2021-05-12T02:41:53Z&se=2021-05-12T10:41:53Z&skoid=20f6bc0d-d946-42b5-ba49-5dffca5ee19a&sktid=159f3555-6979-4668-a816-ea83e6f9f9be&skt=2021-05-12T02:41:53Z&ske=2021-05-12T10:41:53Z&sks=b&skv=2020-02-10&spr=https&sv=2020-02-10&sr=c&sig=uvlZ0UYJu0bu2clTSUakrE73z3j93INmKLLfPVHTXYc%3D")
 blob_client = container_client.get_blob_client(blob ="001.jpg")
 blob_client.upload_blob(image_stream.read(), blob_type="BlockBlob")
-
-
-
